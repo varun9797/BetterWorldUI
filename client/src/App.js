b0ac5153b81c726ca2c0a691cdd9940e31f05a15
Varun Verma <verma9797@gmail.com>
2018-09-22 15:22:16 +0530

    another changes

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React changes done</h1>
+          <h1 className="App-title">Welcome to React changes done great</h1>
         </header>
         <p className="App-intro">{this.state.response}</p>
       </div>
