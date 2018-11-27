35b66e2a193eb16f0d6b82a2f671c12bebc08c1e
verma9797 <verma9797@gmail.com>
2018-11-27 16:37:38 +0530

    path setting

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/myapp'));
+app.use(express.static('dist/myapp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('./dist/myapp/index.html'));
+res.sendFile(path.join('dist/myapp/index.html'),{ root: '.' });
 });
 
 // Start the app by listening on the default Heroku port
