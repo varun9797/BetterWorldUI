9a08d7f2fa2838f3337ffcb1ea90c865894bd862
Varun Verma <verma9797@gmail.com>
2019-02-25 23:54:10 +0530

    changes in login user info while validating token

--- server/modules/users/validator/userValidator.js
+++ server/modules/users/validator/userValidator.js
@@ -27,14 +27,20 @@ class UserValidator {
         var token = req.body.token || req.headers['token'];
         var appData = {};
         if (token) {
-            jwt.verify(token, process.env.SECRET_KEY, function(err) {
+            jwt.verify(token, process.env.SECRET_KEY, function(err, data) {
                 if (err) {
                     appData['error'] = 1;
                     appData['data'] = 'Token is invalid';
                     res.status(500).json(appData);
                 } else {
                     appData['error'] = 0;
-                    appData['data']='Token is valid';
+                    appData['data']={
+                        ownerid:data.ownerid,
+                        ownername:data.ownername,
+                        isadmin:data.isadmin,
+                        phoneNumber:data.phoneNumber,
+                        email:data.email
+                    };
                     res.status(200).json(appData);
                 }
             });
